4150b850fca268530a2f436dfc61eafd3c96be14
Cloyd Van <abcd123.letters@gmail.com>
2024-03-01 01:34:43 +0800

    Reusable Header
    Header.js has now text and size properties

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import CustomNavbar from './components/Navbar';
+import Header from './components/Header';
+
 function App() {
   return (
     <div>
       <CustomNavbar />
+      <Header 
+      text={"Hello, World!"}
+      size={6}/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
 
-function Header(){
+function Header({ text, size }){
     return(
-      <header>Sample header</header>
+      <div className="text-center">
+        {/* The size property renders different sizes from 1 to 6 
+        https://getbootstrap.com/docs/5.0/content/typography/#display-headings*/}
+        <h1 className={`display-${size} fw-bold text-body-emphasis`}>{text}</h1>
+      </div>
     )
   }
   
