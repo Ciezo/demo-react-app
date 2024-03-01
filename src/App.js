ee939a5e381442cfafd3b22503f550159e1e2e1d
Cloyd Van <abcd123.letters@gmail.com>
2024-03-01 15:32:56 +0800

    Major implementations
    Refactor and clean App.js
    Added routing
    Added a directory for pages to integrate routing
    New pages:
     - Home.js
     - Signup.js
     - Login.js

--- src/App.js
+++ src/App.js
@@ -1,104 +1,25 @@
 import CustomNavbar from "./components/Navbar";
-import WelcomeCard from "./components/WelcomeCard";
-import Cards from "./components/Cards";
 import Footer from "./components/Footer";
+
+// App routing
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div>
-      <CustomNavbar />
-      <div className="container-sm mx-auto pt-5">
-        <WelcomeCard />
-        <Cards
-          header={"Unleash Your Creativity"}
-          body={
-            "With Inkdown, the power to customize your content is in your hands. Emphasize your thoughts with bold and italics, create organized lists with numbering, and add links to your texts for a seamless experience. Editing your content is a breeze!"
-          }
-          isRowReverse={false}
-        />
-
-        <Cards
-          header={"Always Remembered"}
-          body={
-            "Inkdown values your work and ensures that your notes are persistently saved, ready to be accessed whenever you need them. Say goodbye to losing your ideas and hello to a reliable companion that keeps track of your thoughts."
-          }
-          isRowReverse={true}
-        />
-
-        <Cards
-          header={"Lightning-Fast Data Retrieval"}
-          body={
-            "Powered by React.js cutting-edge lightweight and dynamic capabilities, Inkdown fetches your notes in the blink of an eye. Say goodbye to waiting and hello to instant access!"
-          }
-          isRowReverse={false}
-        />
-
-        <Cards
-          header={"Seamless 24/7 Access"}
-          body={
-            "Inkdown deploys a remote database pipeline, guaranteeing 24/7 access for all users. Your notes are always within reach, no matter where you are."
-          }
-          isRowReverse={true}
-        />
-
-        <Cards
-          header={"Scale with Ease"}
-          body={
-            "Our source code is designed with scalability in mind, using Classes and Controllers to collect events and user data. Inkdown grows with you, accommodating your needs as they evolve."
-          }
-          isRowReverse={true}
-        />
-
-        <Cards
-          header={"Your Space, Your Way"}
-          body={
-            "We believe in user-centric design, and that's why Inkdown ensures that each user has access to their own respective pages. Enjoy a seamless and personalized experience every time."
-          }
-          isRowReverse={false}
-        />
-
-        <Cards
-          header={"Elegance and Intuition"}
-          body={
-            "Inkdown's user interface is elegantly simple yet remarkably intuitive and dynamic. We understand the value of aesthetics, making your note-taking experience a pleasure."
-          }
-          isRowReverse={true}
-        />
-
-        <Cards
-          header={"A Symphony of Functionality"}
-          body={
-            "Inkdown offers a comprehensive CRUD system, operating on a PHP local API. This wrapper ensures flawless logic and data handling, giving you complete control over your content."
-          }
-          isRowReverse={false}
-        />
-
-        <Cards
-          header={"Archive and Restore"}
-          body={
-            "Never fear losing your valuable notes again! Inkdown implements cutting-edge techniques for archiving and restoring. Safeguard your work and easily restore it whenever needed."
-          }
-          isRowReverse={true}
-        />
-
-        <Cards
-          header={"Save, Edit, Remove, and Restore Notes"}
-          body={
-            "Inkdown has ways to create, edit, and delete personalized notes whilst maintaining the ability to archive and restore them. With Inkdown, the power to customize your content is in your hands. Emphasize your thoughts with bold and italics, create organized lists with numbering, and add links to your texts for a seamless experience. Editing your content is a breeze!  "
-          }
-          isRowReverse={false}
-        />
-
-        <Cards
-          header={"The future of note taking"}
-          body={
-            "Embrace the future of note-taking with Inkdown, where speed, creativity, and reliability converge to give you the ultimate writing experience. Say hello to a world of seamless personalization and innovation today!"
-          }
-          isRowReverse={true}
-        />
+    <Router>
+      <div className="content">
+        <CustomNavbar />
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/signup" Component={Signup} />
+          <Route path="/login" Component={Login} />
+        </Routes>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,44 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Outlet, Link } from "react-router-dom";
 
 function CustomNavbar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme="dark">
-      <Container>
-        <Navbar.Brand href="#home">✒️ Inkdown</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#signup">Signup</Nav.Link>
-            <Nav.Link href="#login">Login</Nav.Link>
-            <NavDropdown title="More" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#license">License</NavDropdown.Item>
-              <NavDropdown.Item href="#terms_and_privacy">Terms & Privacy</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#code_of_conduct">Code of Conduct</NavDropdown.Item>
-              <NavDropdown.Item href="#contribute">Contribute</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#github">GitHub</Nav.Link>
-            <Nav.Link href="#docs">Docs</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">✒️ Inkdown</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
+              <Nav.Link as={Link} to="/login">Login</Nav.Link>
+              <NavDropdown title="More" id="collapsible-nav-dropdown">
+                <NavDropdown.Item href="#license">License</NavDropdown.Item>
+                <NavDropdown.Item href="#terms_and_privacy">
+                  Terms & Privacy
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#code_of_conduct">
+                  Code of Conduct
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#contribute">
+                  Contribute
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#github">GitHub</Nav.Link>
+              <Nav.Link href="#docs">Docs</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </>
   );
 }
 
