51060b25f739980673d59cfd20c875d15c67cf7c
Cloyd Van <abcd123.letters@gmail.com>
2024-03-02 20:18:44 +0800

    Links in Footer
    Added links and routing to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,41 +1,27 @@
 import React from "react";
+import { Outlet, Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer>
       <div className="mx-auto px-5">
-        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-          <li class="nav-item">
-            <a href="#license" class="nav-link px-2 text-body-secondary">
-              License
-            </a>
+        <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+          <li className="nav-item">
+            <Link to="/license" className="nav-link px-2 text-body-secondary">License</Link>
           </li>
-          <li class="nav-item">
-            <a href="#conduct" class="nav-link px-2 text-body-secondary">
-              Code of conduct
-            </a>
+          <li className="nav-item">
+            <Link to="/code-of-conduct" className="nav-link px-2 text-body-secondary">Code of Conduct</Link>
           </li>
-          <li class="nav-item">
-            <a href="#terms" class="nav-link px-2 text-body-secondary">
-              Terms
-            </a>
+          <li className="nav-item">
+            <Link to="/terms-and-privacy" className="nav-link px-2 text-body-secondary">Terms and Privacy</Link>
           </li>
-          <li class="nav-item">
-            <a href="#privacy" class="nav-link px-2 text-body-secondary">
-              Privacy
-            </a>
-          </li>
-          <li class="nav-item">
-            <a
-              href="#how_data_is_collected"
-              class="nav-link px-2 text-body-secondary"
-            >
-              How we collect data?
-            </a>
+          <li className="nav-item">
+            <Link to="/data-collection" className="nav-link px-2 text-body-secondary">How we collect data?</Link>
           </li>
         </ul>
-        <p class="text-center">&copy; 2024 Cloyd Van Secuya </p>
+        <p className="text-center">&copy; 2024 Cloyd Van Secuya </p>
       </div>
+      <Outlet />
     </footer>
   );
 }
