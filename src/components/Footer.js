1eb03f43584ed474028bf6c4b6f700edae5087e0
Cloyd Van <abcd123.letters@gmail.com>
2024-03-02 20:57:13 +0800

    Footer links animations

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,16 +7,16 @@ function Footer() {
       <div className="mx-auto px-5">
         <ul className="nav justify-content-center border-bottom pb-3 mb-3">
           <li className="nav-item">
-            <Link to="/license" className="nav-link px-2 text-body-secondary">License</Link>
+            <Link to="/license" className="nav-link px-2 text-body-secondary" id="links">License</Link>
           </li>
           <li className="nav-item">
-            <Link to="/code-of-conduct" className="nav-link px-2 text-body-secondary">Code of Conduct</Link>
+            <Link to="/code-of-conduct" className="nav-link px-2 text-body-secondary" id="links">Code of Conduct</Link>
           </li>
           <li className="nav-item">
-            <Link to="/terms-and-privacy" className="nav-link px-2 text-body-secondary">Terms and Privacy</Link>
+            <Link to="/terms-and-privacy" className="nav-link px-2 text-body-secondary" id="links">Terms and Privacy</Link>
           </li>
           <li className="nav-item">
-            <Link to="/data-collection" className="nav-link px-2 text-body-secondary">How we collect data?</Link>
+            <Link to="/data-collection" className="nav-link px-2 text-body-secondary" id="links">How we collect data?</Link>
           </li>
         </ul>
         <p className="text-center">&copy; 2024 Cloyd Van Secuya </p>
