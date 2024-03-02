6bc7faed7f4b3a2eab59c269acfea161816ba4e5
Cloyd Van <abcd123.letters@gmail.com>
2024-03-02 08:51:40 +0800

    Page updated LicenseCard

--- src/pages/License.js
+++ src/pages/License.js
@@ -1,11 +1,14 @@
 import React from "react";
+import LicenseCard from "../components/LicenseCard";
 
 function License() {
-    return(
-        <>
-            <div>License</div>
-        </>
-    );
+  return (
+    <>
+      <div className="container-sm mx-auto pt-5">
+        <LicenseCard />
+      </div>
+    </>
+  );
 }
 
 export default License;
