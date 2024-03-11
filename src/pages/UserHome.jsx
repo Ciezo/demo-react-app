ecbdd3742c2b2b6f1b2ec73aa6b592fb7197d8cf
Cloyd Van <abcd123.letters@gmail.com>
2024-03-11 13:13:46 +0800

    Fix id assignment for NotesCard

--- src/pages/UserHome.jsx
+++ src/pages/UserHome.jsx
@@ -68,7 +68,7 @@ function UserHome() {
                 {notes.map((note, index) => (
                   <NotesCard
                     key={index}
-                    id={index}
+                    id={note.id}
                     title={note.title}
                     body={note.body}
                     author={note.author}
