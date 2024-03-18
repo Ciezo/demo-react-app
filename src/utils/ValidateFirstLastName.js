d155aa8890b6a388583136a867159890db550da3
Cloyd Van <abcd123.letters@gmail.com>
2024-03-18 18:13:33 +0800

    Update rule

--- src/utils/ValidateFirstLastName.js
+++ src/utils/ValidateFirstLastName.js
@@ -2,7 +2,7 @@ export const validateFirstLastName = (name) => {
   if (!name) return "First or last name is required";
   if (/\d/.test(name)) return "First or last names should not contain numbers";
   if (name.length < 5) return "First or last names must be at least 5 characters";
-  if (name.length > 10) return "First or last names should not be more than 10 characters";
+  if (name.length > 255) return "First or last names should not exceed 255 characters";
 
   return; // No error
 };
