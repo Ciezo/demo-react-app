f290d667954bea4bc607a00e0bf557f26aa84415
Cloyd Van <abcd123.letters@gmail.com>
2024-03-23 19:11:40 +0800

    Minor changes
    Integrated async-await method

--- src/test/ExtractUserIdPrimaryKey.jsx
+++ src/test/ExtractUserIdPrimaryKey.jsx
@@ -1,17 +1,21 @@
-import React from "react";
-import { getUserIdPrimaryKey } from "../utils/GetUserIdPrimaryKey";
+import React, { useState } from "react";
 import { extract_auth_state } from "../utils/ExtractAuthState";
+import getUserIdPrimaryKey from "../utils/GetUserIdPrimaryKey";
 
 function TestExtractUserIdPrimaryKey() {
     const username = extract_auth_state("_auth_state");
-    let userId = getUserIdPrimaryKey(username.user)
+    const [userId, setUserId] = useState("");
     
-    console.log(userId);
-
+    const getUserId = async () => {
+        const user_user_id = await getUserIdPrimaryKey(username.user);
+        setUserId(user_user_id);
+    }; getUserId();
+    
     return (
         <>
             <div className="lead">Check browser console...</div>
             <div className="lead">Check browser network tab... then response..</div>
+            <div className="lead"> UserId Primary key: {userId} </div>
         </>
     );
 
--- src/utils/GetUserIdPrimaryKey.js
+++ src/utils/GetUserIdPrimaryKey.js
@@ -1,28 +1,29 @@
 import { extract_auth } from "./ExtractAuth";
 /**
- * Extracts the `user_id` primary key from the `User` entity table
+ * A simple async-function to extract the `user_id` primary key from the `User` entity table
+ * <b>No problem using with non-functional level components</b>
  * @param {string} username
  * @return userId a primary key to identify a `User` row from `User` entity
  */
-export function getUserIdPrimaryKey(username) {
+async function getUserIdPrimaryKey(username) {
   const baseURL = "http://localhost:18080/api/inkdown/v1";
-  const token = extract_auth();
-  let userId = null;
+  const token = extract_auth(); // Assuming you have a function called extract_auth()
 
-  const getUserId = async () => {
-    try {
-      const response = await fetch(baseURL + `/user/${username}`, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
+  try {
+    const response = await fetch(baseURL + `/user/${username}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
 
-      if (response.ok) userId = await response.json();
-      
-    } catch (error) {
-      console.log("Something went wrong..");
-      console.error(error);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      console.error(
+        `Error fetching data: ${response.status} ${response.statusText}`
+      );
     }
-  };
-  getUserId();
-
-  return userId;
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 }
+export default getUserIdPrimaryKey;
