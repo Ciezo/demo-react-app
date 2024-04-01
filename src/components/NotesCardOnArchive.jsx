bb08f982ed9a95d3984a961c86dc0dd8b0358cef
Cloyd Van <abcd123.letters@gmail.com>
2024-04-01 21:09:37 +0800

    Major updates and implementation
    Integrated Spring Boot api calls

--- src/components/NotesCardOnArchive.jsx
+++ src/components/NotesCardOnArchive.jsx
@@ -14,30 +14,58 @@ import Card from "react-bootstrap/Card";
 import { Col } from "react-bootstrap";
 import { MdRestorePage } from "react-icons/md"
 import { Button, ButtonGroup } from "react-bootstrap";
+import { extract_auth_state } from "../utils/ExtractAuthState";
+import { extract_auth } from "../utils/ExtractAuth"
 
 function NotesCardOnArchive(props) {
+  const _user = extract_auth_state("_auth_state");
+  const token = extract_auth();
+  const baseURL = "http://localhost:18080/api/inkdown/v1";
   const { id, title, body, author } = props;
   const note = { id, title, body, author };
+  const [isNoteRestored, setNoteCardVisibility] = useState(false);
+  
+  const user = {
+    id: _user.user_id_pk,
+    role: "USER"
+  };
+
+  const postObjectNewNote = {
+    note, 
+    user
+  }
 
   // Remove the note card component once restored back to the home page
-  const [isNoteRestored, setNoteCardVisibility] = useState(false);
-  const restoreNote = () => {
-    fetch('http://localhost:8001/notes', {
-        method: 'POST',
-        headers: { 'Content-Type' : 'application/json' },
-        body: JSON.stringify(note) 
-      })
-      .then(() => { setNoteCardVisibility(true); })
-      // Remove the original archived note from this resource
-      .then(() => {
-        fetch("http://localhost:8001/notes-archive/" + note.id, {
+  const restoreNote = async () => {
+    try {
+      // Create a copy of an archive note to send to /notes  
+      const createNewNoteCopy = JSON.stringify(postObjectNewNote);
+      await fetch(baseURL + "/notes", {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${token}`,
+          "Content-Type": "application/json"
+        },
+        body: createNewNoteCopy
+      }).then(() => {
+        // Remove the original archive note from its own resource, /notes-archive
+        fetch(baseURL + "/notes-archive/delete-archive-note/id/" + note.id, {
           method: "DELETE",
+          headers: { 
+            "Authorization": `Bearer ${token}` 
+          }
         }).catch((error) => {
-          console.log("Cannot delete original note from /notes endpoint");
-          console.log(error)
+          console.log("Something went wrong with the deletion of archive note");
+          console.error(error);
         })
-      })
-      .catch((error) => { console.error(error); });
+      });
+      
+      setNoteCardVisibility(true);
+
+    } catch (error) {
+      console.log("Something went wrong with deleting the archive note");
+      console.error(error);
+    }
   };
 
   return (
@@ -46,13 +74,14 @@ function NotesCardOnArchive(props) {
         <Col xs={3} className="mb-3">
           <Card style={{ width: "15rem" }}>
             <Card.Body>
-              <Card.Title>{title}</Card.Title>
+              <Card.Title>{note.title}</Card.Title>
               <small>
                 <p>
-                  <i>By: {author}</i>
+                  <i>By: {note.author}</i>
                 </p>
               </small>
-              <Card.Text>{body}</Card.Text>
+              <p>id: {note.id} </p>
+              <Card.Text>{note.body}</Card.Text>
             </Card.Body>
             <ButtonGroup className="mx-auto my-2">
               <Button variant="outline-info" onClick={restoreNote}>
