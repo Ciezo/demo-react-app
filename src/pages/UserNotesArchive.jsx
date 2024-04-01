8b4edfd03cbb4bdac2c8d7326f28c0780c5e6368
Cloyd Van <abcd123.letters@gmail.com>
2024-04-01 18:41:03 +0800

    Fetching archive notes based on current user
    Implemented a simple filtering to render and assign all archive notes based on the user who is logged-in
    
    Added doc comments and further explanations

--- src/pages/UserNotesArchive.jsx
+++ src/pages/UserNotesArchive.jsx
@@ -1,32 +1,70 @@
+import React from "react";
 import { useEffect, useState } from "react";
 import UserHomeNavbar from "../components/UserHomeNavbar";
 import { Container, Row, Col } from "react-bootstrap";
 import Sidebar from "../components/Sidebar";
 import { extract_auth_state } from "../utils/ExtractAuthState";
 import NotesCardOnArchive from "../components/NotesCardOnArchive";
+import { extract_auth } from "../utils/ExtractAuth";
+import useFetch from "../api/useFetch";
 
 function UserNotesArchive() {
-  const [username, setUsername] = useState("");
   const [archiveNotes, setArchiveNotes] = useState([]);
   const [isError, setError] = useState(false);
-
+  const user = extract_auth_state("_auth_state");
+  const token = extract_auth();
+  const { data, isPending, error } = useFetch(`/notes-archive`, token);
+  
   useEffect(() => {
-    const username = extract_auth_state("_auth_state");
-    setUsername(username.user);
-  }, []);
+    try {
+      if(data) {
+        /** All archived notes by its own username 
+         * should be assigned here... 
+         * 
+         * `/notes-archive` is returning all kinds of archived notes from all users.
+         * 
+         * Therefore, we want to create a match to assign all archived notes 
+         * by the same user who is logged-in.
+         * 
+         * */
+        const archiveNotesByUsername = [];
 
-  // Check the :8001/notes-archive endpoint if there are any archived notes
-  useEffect(() => {
-    fetch("http://localhost:8001/notes-archive")
-      .then((res) => res.json())
-      .then((data) => {
-        setArchiveNotes(data);
-      })
-      .catch((error) => {
-        setError(true);
-        console.error("Error fetching data: " + error);
-      });
-  }, []);
+        for(const notesArchive of data) {
+          /** The supposed author of an archive note */
+          const _user = notesArchive.author;
+          /** Current user logged-in */
+          const userInSession = user.user;
+          
+          const id = notesArchive.id;
+          const title = notesArchive.title;
+          const author = notesArchive.author;
+          const body = notesArchive.body;
+
+          const notesOnArchiveObject = {
+            id, 
+            title,
+            author,
+            body
+          };
+
+          /** Match and assign all archive notes data related to the 
+           * User by using the username value matched to the author of the archive note
+           * To put it simply, we just want to filter the data...
+           */
+          if(_user === userInSession) {   // A filter condition
+            archiveNotesByUsername.push(notesOnArchiveObject);
+            setArchiveNotes(archiveNotesByUsername);
+          }
+        }
+
+      }
+
+    } catch (error) {
+      setError(error);
+      console.log("An error occurred!"); 
+      throw new Error(error);
+    }
+  }, [data, user.user]);
 
   return (
     <>
@@ -40,8 +78,18 @@ function UserNotesArchive() {
           </Col>
 
           <Col lg={10} className="mx-auto my-2">
-            <p className="lead">Archived notes by {username}</p>
-            {isError && <p className="lead">An error occurred no archive notes found...</p>}
+            <p className="lead">Archived notes by {user.user}</p>
+
+            {/* Error prompt */}
+            {isError && <div className="lead">
+              An error occurred! 
+              <div className="lead"> { error }</div>
+            </div>}
+
+            {/* Loading Prompt */}
+            {isPending && <div className="lead">Loading...</div>}
+            
+            {/* Render the archive notes here */}
             <Container fluid>
               {!isError && (
                 <Row className="p-2 mx-auto">
